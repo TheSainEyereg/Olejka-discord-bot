799b8a799a5f6189c74ddbed16ffccbb6153ef5b
Oleg Logvinov <TheSainEyereg@users.noreply.github.com>
2021-02-18 14:44:00 +0300

    First code

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-//Main JS file
+const Discord = require("discord.js");
+const config = require("./config.json");
+const prefix = config.prefix
+
+const bot = new Discord.Client();
+
+bot.on('message', message => {
+    if (message.author.bot) return
+    if (!message.content.startsWith(prefix)) return;
+
+    const body = message.content.slice(prefix.length);
+    const args = body.split(' ');
+    const command = args.shift().toLowerCase();
+
+})
+
+bot.login(config.token);
