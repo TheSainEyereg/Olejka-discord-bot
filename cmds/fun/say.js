5a7f17b2b61f9405a285f348382a875cf583fc87
Oleg Logvinov <TheSainEyereg@users.noreply.github.com>
2021-02-19 23:09:21 +0300

    "Critical" error fix

--- cmds/fun/say.js
+++ cmds/fun/say.js
@@ -3,6 +3,7 @@ module.exports = {
 	description: 'Repeat given word',
 	arguments: '[word to say]',
 	execute(message, args) {
+		if (!args[0]) {message.channel.send('No args provided'); return}
         message.channel.send(args[0]);        
 	}
 };
