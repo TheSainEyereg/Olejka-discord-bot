810a6777222bb8e218216255921670f175473d6b
Oleg Logvinov <TheSainEyereg@users.noreply.github.com>
2021-02-22 22:04:27 +0300

    Fun commands now work

--- cmds/fun/communism.js
+++ cmds/fun/communism.js
@@ -1,8 +1,27 @@
+const Discord = require('discord.js');
+const Canvas = require('canvas');
+
 module.exports = {
 	name: 'communism',
 	description: 'Stolen idea from BoobBot',
 	arguments: '(user)',
-	execute(message, args) {
-        //later
+	async execute(message, args) {
+		let user = message.guild.member(message.mentions.users.first() || message.guild.members.fetch(args[0]));
+
+		const canvas = Canvas.createCanvas(256, 256);
+		const ctx = canvas.getContext('2d');
+
+		const avatar = await Canvas.loadImage(user.user.displayAvatarURL({ format: 'jpg' }));
+		const background = await Canvas.loadImage('https://olejka.ru/s/63674.png');
+
+		ctx.drawImage(avatar, 0, 0, canvas.width, canvas.height);
+		ctx.globalAlpha = 0.6
+		ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+		ctx.globalAlpha = 1
+
+		const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'communism.png');
+		message.channel.send(attachment);
 	}
 };
+
+//https://olejka.ru/s/63674.png
--- cmds/fun/rip.js
+++ cmds/fun/rip.js
@@ -1,8 +1,30 @@
+const Discord = require('discord.js');
+const Canvas = require('canvas');
+
 module.exports = {
 	name: 'rip',
 	description: 'Kills person >:)',
 	arguments: '(user)',
-	execute(message, args) {
-        //later
+	async execute(message, args) {
+		let user = message.guild.member(message.mentions.users.first() || message.guild.members.fetch(args[0]));
+
+		const canvas = Canvas.createCanvas(653, 425);
+		const ctx = canvas.getContext('2d');
+		ctx.font = '40px sans-serif';
+		ctx.fillStyle = '#010101';
+
+		const background = await Canvas.loadImage('https://olejka.ru/s/92385.jpg');
+		const avatar = await Canvas.loadImage(user.user.displayAvatarURL({ format: 'jpg' }));
+		let t = new Date()
+
+		ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+		ctx.drawImage(avatar, canvas.width/2-125/2-20, 80, 125, 125);
+		ctx.fillText(user.displayName, canvas.width/2-24*user.displayName.length/2, 260);
+		ctx.fillText(`${t.getDate()}.${t.getMonth()+1}.${t.getFullYear()}`,canvas.width/2-22*10/2, 300);
+
+		const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'Tombstone.png');
+		message.channel.send(attachment);
 	}
-};
+}
+
+//https://olejka.ru/s/92385.jpg
