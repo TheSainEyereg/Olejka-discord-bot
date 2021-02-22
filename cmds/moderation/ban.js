0ca495176485798bb6bb74ace37e9c29036d9250
Oleg Logvinov <TheSainEyereg@users.noreply.github.com>
2021-02-22 16:30:08 +0300

    ban/kick fix for discord.js v12

--- cmds/moderation/ban.js
+++ cmds/moderation/ban.js
@@ -1,28 +1,29 @@
-const logs = require("../../config.json").logs;
+const Discord = require('discord.js');
+const logs = require('../../config.json').logs;
 
 module.exports = {
-    name: "ban",
-    description: "Bans member from server",
-    arguments: "[user] (reason)",
+    name: 'ban',
+    description: 'Bans member from server',
+    arguments: '[user] (reason)',
     async execute(message, args) {
         let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.fetch(args[0]));
-        if (!bUser) return message.channel.send("Can't find user!");
+        if (!bUser) return message.channel.send('Can\'t find user!');
         let bReason = args[1];
-        if (!message.member.hasPermission("MANAGE_MEMBERS"))
-            return message.channel.send("You have no permissions to ban/ban!");
-        if (bUser.hasPermission("MANAGE_MESSAGES"))
-            return message.channel.send("That person can't be banned!");
+        if (!message.member.hasPermission('MANAGE_MEMBERS'))
+            return message.channel.send('You have no permissions to ban/ban!');
+        if (bUser.hasPermission('MANAGE_MESSAGES'))
+            return message.channel.send('That person can\'t be banned!');
 
-        let banEmbed = new Discord.RichEmbed()
-            .setDescription("~Ban~")
-            .setColor("#bc0000")
-            .addField("Banned User", `${bUser} with ID ${bUser.id}`)
-            .addField("Banned By",`<@${message.author.id}> with ID ${message.author.id}` )
-            .addField("Banned In", message.channel)
-            .addField("Time", message.createdAt)
-            .addField("Reason", bReason);
+        let banEmbed = new Discord.MessageEmbed()
+            .setDescription('~Ban~')
+            .setColor('#bc0000')
+            .addField('Banned User', `${bUser} with ID ${bUser.id}`)
+            .addField('Banned By',`<@${message.author.id}> with ID ${message.author.id}` )
+            .addField('Banned In', message.channel)
+            .addField('Time', message.createdAt)
+            .addField('Reason', bReason);
 
-        let banChannel = message.guild.channels.find(`name`, logs);
+        let banChannel = message.guild.channels.cache.find(c => c.name === logs);
         if (banChannel) banChannel.send(banEmbed);
 
         message.guild.member(bUser).ban(bReason);
--- cmds/moderation/kick.js
+++ cmds/moderation/kick.js
@@ -1,28 +1,29 @@
-const logs = require("../../config.json").logs;
+const Discord = require('discord.js');
+const logs = require('../../config.json').logs;
 
 module.exports = {
-    name: "kick",
-    description: "Kicks member from server",
-    arguments: "[user] (reason)",
+    name: 'kick',
+    description: 'Kicks member from server',
+    arguments: '[user] (reason)',
     execute(message, args) {
         let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.fetch(args[0]));
-        if (!kUser) return message.channel.send("Can't find user!");
+        if (!kUser) return message.channel.send('Can\'t find user!');
         let kickReason = args[1];
-        if (!message.member.hasPermission("MANAGE_MESSAGES"))
-            return message.channel.send("You have no permissions to ban/kick!");
-        if (kUser.hasPermission("MANAGE_MESSAGES"))
-            return message.channel.send("That person can't be kicked!");
+        if (!message.member.hasPermission('MANAGE_MESSAGES'))
+            return message.channel.send('You have no permissions to ban/kick!');
+        if (kUser.hasPermission('MANAGE_MESSAGES'))
+            return message.channel.send('That person can\'t be kicked!');
 
-        let kickEmbed = new Discord.RichEmbed()
-            .setDescription("~Kick~")
-            .setColor("#e56b00")
-            .addField("Kicked User", `${kUser} with ID ${kUser.id}`)
-            .addField("Kicked By", `<@${message.author.id}> with ID ${message.author.id}`)
-            .addField("Kicked In", `${message.channel}`)
-            .addField("Tiime", `${message.createdAt}`)
-            .addField("Reason", kickReason);
+        let kickEmbed = new Discord.MessageEmbed()
+            .setDescription('~Kick~')
+            .setColor('#e56b00')
+            .addField('Kicked User', `${kUser} with ID ${kUser.id}`)
+            .addField('Kicked By', `<@${message.author.id}> with ID ${message.author.id}`)
+            .addField('Kicked In', `${message.channel}`)
+            .addField('Tiime', `${message.createdAt}`)
+            .addField('Reason', kickReason);
 
-        let kickChannel = message.guild.channels.find(`name`, logs);
+        let kickChannel = message.guild.channels.cache.find(c => c.name === logs);
         if (kickChannel) kickChannel.send(kickEmbed);
 
         message.guild.member(kUser).kick(kickReason);
