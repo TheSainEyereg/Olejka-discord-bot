f6037d5e79df31db236a3e30b95c6101ea74c84b
Oleg Logvinov <TheSainEyereg@users.noreply.github.com>
2021-02-22 14:54:46 +0300

    Formated code

--- cmds/moderation/ban.js
+++ cmds/moderation/ban.js
@@ -1,28 +1,30 @@
-const logs = require('../../config.json').logs
+const logs = require("../../config.json").logs;
 
 module.exports = {
-	name: 'ban',
-	description: 'Bans member from server',
-	arguments: '[user] (reason)',
-	async execute(message, args) {
-		let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.fetch(args[0]));
-		if(!bUser) return message.channel.send('Can\'t find user!');
-		let bReason = args[1]
-		if(!message.member.hasPermission('MANAGE_MEMBERS')) return message.channel.send('You have no permissions to ban/ban!');
-		if(bUser.hasPermission('MANAGE_MESSAGES')) return message.channel.send('That person can\'t be banned!');
-	
-		let banEmbed = new Discord.RichEmbed()
-		.setDescription('~Ban~')
-		.setColor('#bc0000')
-		.addField('Banned User', `${bUser} with ID ${bUser.id}`)
-		.addField('Banned By', `<@${message.author.id}> with ID ${message.author.id}`)
-		.addField('Banned In', message.channel)
-		.addField('Time', message.createdAt)
-		.addField('Reason', bReason);
-	
+    name: "ban",
+    description: "Bans member from server",
+    arguments: "[user] (reason)",
+    async execute(message, args) {
+        let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.fetch(args[0]));
+        if (!bUser) return message.channel.send("Can't find user!");
+        let bReason = args[1];
+        if (!message.member.hasPermission("MANAGE_MEMBERS"))
+            return message.channel.send("You have no permissions to ban/ban!");
+        if (bUser.hasPermission("MANAGE_MESSAGES"))
+            return message.channel.send("That person can't be banned!");
+
+        let banEmbed = new Discord.RichEmbed()
+            .setDescription("~Ban~")
+            .setColor("#bc0000")
+            .addField("Banned User", `${bUser} with ID ${bUser.id}`)
+            .addField("Banned By",`<@${message.author.id}> with ID ${message.author.id}` )
+            .addField("Banned In", message.channel)
+            .addField("Time", message.createdAt)
+            .addField("Reason", bReason);
+
         let banChannel = message.guild.channels.find(`name`, logs);
-        if(banChannel) banChannel.send(banEmbed);
-	
-		message.guild.member(bUser).ban(bReason);
-	}
+        if (banChannel) banChannel.send(banEmbed);
+
+        message.guild.member(bUser).ban(bReason);
+    },
 };
--- cmds/moderation/mute.js
+++ cmds/moderation/mute.js
@@ -1,64 +1,62 @@
-const ms = require('ms');
-const logs = require('../../config.json').logs
+const ms = require("ms");
+const logs = require("../../config.json").logs;
 
 module.exports = {
-	name: 'mute',
-	description: 'Mutes member on server',
-	arguments: '[user] (duration (mins)) (reason)',
-	async execute(message, args) {
-		if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send('You do not have permissions to use this command');
+    name: "mute",
+    description: "Mutes member on server",
+    arguments: "[user] (duration (mins)) (reason)",
+    async execute(message, args) {
+        if (!message.member.hasPermission("MANAGE_MESSAGES"))
+            return message.channel.send("You do not have permissions to use this command");
 
         let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.fetch(args[0]));
-        if(!tomute) return message.channel.send('Couldn\'t find user.');
-        if(tomute.hasPermission('MANAGE_MESSAGES')) return message.channel.send('Can\'t mute them!');
-        let muterole = message.guild.roles.find(`name`, 'Muted');
-        if(!muterole){
-          try{
-            muterole = await message.guild.createRole({
-              name: 'Muted',
-              color: '#000000',
-              permissions:[]
-            })
-            message.guild.channels.forEach(async (channel, id) => {
-              await channel.overwritePermissions(muterole, {
-                SEND_MESSAGES: false,
-                ADD_REACTIONS: false
-              });
-            });
-          }catch(e){
-            console.log(e.stack);
-          }
+        if (!tomute) return message.channel.send("Couldn't find user.");
+        if (tomute.hasPermission("MANAGE_MESSAGES"))
+            return message.channel.send("Can't mute them!");
+        let muterole = message.guild.roles.find(`name`, "Muted");
+        if (!muterole) {
+            try {
+                muterole = await message.guild.createRole({
+                    name: "Muted",
+                    color: "#000000",
+                    permissions: [],
+                });
+                message.guild.channels.forEach(async (channel, id) => {
+                    await channel.overwritePermissions(muterole, {
+                        SEND_MESSAGES: false,
+                        ADD_REACTIONS: false,
+                    });
+                });
+            } catch (e) {
+                console.log(e.stack);
+            }
         }
-        
-        let mutetime = args[1]
-        let reason = args[2]
-        if (!parseInt(mutetime)) {mutetime = 'Permanent'; reason = args[1]}
 
-	  
+        let mutetime = args[1];
+        let reason = args[2];
+        if (!parseInt(mutetime)) { mutetime = "Permanent"; reason = args[1];}
+
         let muteEmbed = new Discord.RichEmbed()
-        .setDescription('~Mute~')
-        .setColor('#e56b00')
-        .addField('Muted User', `${tomute} with ID ${tomute.id}`)
-        .addField('Muted By', `<@${message.author.id}> with ID ${message.author.id}`)
-        .addField('Muted In', `${message.channel}`)
-        .addField('Muted For', `${mutetime}`)
-        .addField('Tiime', `${message.createdAt}`)
-        .addField('Reason', reason);
-        
-        
+            .setDescription("~Mute~")
+            .setColor("#e56b00")
+            .addField("Muted User", `${tomute} with ID ${tomute.id}`)
+            .addField("Muted By", `<@${message.author.id}> with ID ${message.author.id}`)
+            .addField("Muted In", `${message.channel}`)
+            .addField("Muted For", `${mutetime}`)
+            .addField("Tiime", `${message.createdAt}`)
+            .addField("Reason", reason);
+
         let muteChannel = message.guild.channels.find(`name`, logs);
-        if(muteChannel) muteChannel.send(muteEmbed);
-        
-        await(tomute.addRole(muterole.id));
+        if (muteChannel) muteChannel.send(muteEmbed);
+
+        await tomute.addRole(muterole.id);
         message.channel.send(`<@${tomute.id}> has been muted for ${mutetime}`);
 
         if (parseInt(mutetime)) {
-          setTimeout(() => {
-            tomute.removeRole(muterole.id);
-            message.channel.send(`<@${tomute.id}> has been unmuted!`);
-          }, ms(mutetime));
+            setTimeout(() => {
+                tomute.removeRole(muterole.id);
+                message.channel.send(`<@${tomute.id}> has been unmuted!`);
+            }, ms(mutetime));
         }
-        
-        
-	}
+    },
 };
