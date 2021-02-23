b493511e58473fac993c22bc9d9929c5c63d43f3
Oleg Logvinov <TheSainEyereg@users.noreply.github.com>
2021-02-23 13:39:21 +0300

    Created guthub command

---
+++ cmds/utility/github.js
@@ -0,0 +1,8 @@
+module.exports = {
+	name: 'github',
+	description: 'Sends github repo',
+	arguments: '',
+	execute(message) {
+		message.channel.send('https://github.com/TheSainEyereg/Olejka-discord-bot');
+	},
+};
