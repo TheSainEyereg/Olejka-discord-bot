c3c64e74d6bbc067457e0caebdd5da35d7eb743d
Oleg Logvinov <37226226+TheSainEyereg@users.noreply.github.com>
2021-03-15 18:40:29 +0300

    Fixed updater lol

--- cmds/service/update.js
+++ cmds/service/update.js
@@ -10,7 +10,7 @@ module.exports = {
         if (message.author.id != config.creatorid) return message.channel.send(`You are not creator of this bot`)
         exec("git pull", (error, stdout, stderr) => {
             if (error) return message.channel.send(`Error in execute: ${error.message}`)
-            if (stderr) return message.channel.send(`Error in update check: ${stderr}`)
+            //if (stderr) return message.channel.send(`Error in update check: ${stderr}`)
             if (stdout == `Already up to date.\n`) return message.channel.send(`You already up to date.`)
             message.channel.send(`Update found. Reloading commands...`)
             
