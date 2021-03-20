963803a168965a5af5747c364e426070448c733f
Oleg Logvinov <37226226+TheSainEyereg@users.noreply.github.com>
2021-03-20 15:44:45 +0300

    Fixed music volume

--- cmds/music/volume.js
+++ cmds/music/volume.js
@@ -8,8 +8,9 @@ module.exports = {
 		const serverQueue = message.client.queue.get(message.guild.id);
 		if (!serverQueue) return message.channel.send('There is nothing playing.');
 		if (!args[0]) return message.channel.send(`The current volume is: **${serverQueue.volume}**`);
-		if (toString(args[0])>100) return message.channel.send(`Volume ${args[0]} is too big (max volume is 100)`)
-		if (toString(args[0])<1) return message.channel.send(`Volume ${args[0]} is too small (min volume is 1)`)
+		if (!parseInt(args[0])) return message.channel.send('This must be number.');
+		if (parseInt(args[0]) >100) return message.channel.send(`Volume ${args[0]} is too big (max volume is 100)`)
+		if (parseInt(args[0])<1) return message.channel.send(`Volume ${args[0]} is too small (min volume is 1)`)
 		[serverQueue.volume] = args;
 		serverQueue.connection.dispatcher.setVolumeLogarithmic(args[0] /100);
 		return message.channel.send(`I set the volume to: **${args[0]}**`);
