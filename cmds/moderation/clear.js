7a297356a20264a709bb7ac6583262065a803ab3
Oleg Logvinov <37226226+TheSainEyereg@users.noreply.github.com>
2021-03-30 22:58:48 +0300

    Fixed crash issue

--- cmds/moderation/clear.js
+++ cmds/moderation/clear.js
@@ -3,7 +3,7 @@ module.exports = {
     description: 'Clears messages count',
 	arguments: '[count]',
     execute(message, args) {
-        if (!message.member.hasPermission('MANAGE_MESSAGES') || !message.member.hasPermission('ADMINISTRATOR')) return mess.channel.send('You have no permission to remove messages');
+        if (!message.member.hasPermission('MANAGE_MESSAGES') || !message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('You have no permission to remove messages');
         if (!args[0]) return message.channel.send('No amount given')
         const amount = parseInt(args[0])+1 //For command message
         if (isNaN(amount)) return message.channel.send('That is not a number -_-')
