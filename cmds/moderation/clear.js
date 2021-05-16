65214e2b6a8302d4313a1e1d2b3ea22027793bd4
TheSainEyereg <37226226+TheSainEyereg@users.noreply.github.com>
2021-05-16 20:33:11 +0300

    Added moderator check for clear cmd

--- cmds/moderation/clear.js
+++ cmds/moderation/clear.js
@@ -3,7 +3,10 @@ module.exports = {
     description: 'Clears messages count',
 	arguments: '[count]',
     execute(message, args) {
-        if (!message.member.hasPermission('MANAGE_MESSAGES') || !message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('You have no permission to remove messages')
+        if (!message.member.hasPermission('MANAGE_MESSAGES') || !message.member.hasPermission('ADMINISTRATOR')) {
+                const mods = require('../../servers.json')[message.guild.id].moders
+                if (!mods.includes(message.author.id)) return message.channel.send('You do not have permissions to use this command')
+        }
         if (!args[0]) return message.channel.send('No amount given')
         const amount = parseInt(args[0])+1 //For command message
         if (isNaN(amount)) return message.channel.send('That is not a number -_-')
