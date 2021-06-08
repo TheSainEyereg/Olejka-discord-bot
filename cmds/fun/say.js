5a894c506753cc9fc10dd12e4ad1b6bbea80c782
TheSainEyereg <37226226+TheSainEyereg@users.noreply.github.com>
2021-06-08 19:40:48 +0300

    Fixed Artemking4

--- cmds/fun/say.js
+++ cmds/fun/say.js
@@ -3,7 +3,9 @@ module.exports = {
 	description: 'Repeat given word',
 	arguments: '[word to say]',
 	execute(message, args) {
-		if (!args[0]) return message.channel.send('Nothing to say')
-        message.channel.send(args.join(' ').replace(/@/g,''))
+		if (!args[0]) return message.channel.send('Nothing to say');
+		let out = args.join(' ').replace(/@/g,'');
+		if (!out) return message.channel.send('Can\'t send empty message!');
+        message.channel.send(out);
 	}
 };
