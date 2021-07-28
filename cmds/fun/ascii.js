4011c2a809ab16ec37a05b4138a2ade0a3ca0354
TheSainEyereg <37226226+TheSainEyereg@users.noreply.github.com>
2021-07-28 15:33:06 +0300

    Added ASCII art

---
+++ cmds/fun/ascii.js
@@ -0,0 +1,17 @@
+const axios = require('axios');
+
+module.exports = {
+	name: "ascii",
+	description: "Makes ascii art",
+	arguments: "[words to ascii]",
+	execute(message, args) {
+		if (!args[0]) return message.channel.send("Nothing to convert");
+        axios.get(`https://artii.herokuapp.com/make?text=${args.join(" ")}`)
+        .then(response => {
+            message.channel.send(`\`\`\`${response.data}\`\`\``);
+        })
+        .catch(e => {
+            return message.channel.send(`Error in ascii converting: \`\`\`${e}\`\`\``);
+        });
+	}
+};
